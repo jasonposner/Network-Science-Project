9d7e7871f2725f3166e53a0041bd8c91b4259645
fdznoriega <andresfdz7@gmail.com>
2020-04-07 17:13:00 -0400

    added first 25 performers to a separate csv

--- data_formatting.py
+++ data_formatting.py
@@ -1,13 +1,32 @@
 import csv
 
-performers = []
+performerColumn = []
 
 # only fetch the artists
-with open('hot_stuff.csv', newline='\n') as csvfile:
+with open('hot_stuff_25.csv', newline='\n') as csvfile:
      spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
      for row in spamreader:
-         performers.append(row[4])
+         performerColumn.append(row[4])
 
+
+# filter out repeated appearances
+performers = []
+
+for p in performerColumn:
+    if p not in performers and p != 'Performer':
+        performers.append(p)
+
+# sort performers before we put them in the new csv
 performers.sort()
-
 print(performers)
+print(performers[1])
+
+# write a csv that contains only performers
+with open('performers.csv', 'w', newline='\n') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',')
+    index = 0
+    while index < len(performers):
+        spamwriter.writerow([performers[index]])
+        index += 1
+
+# if performers have collaborated, put them in the same row
